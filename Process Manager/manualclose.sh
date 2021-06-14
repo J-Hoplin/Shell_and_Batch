a4bf459053f88cd0f321871fcd735b0f539895bf
Junho Yoon <45956041+J-hoplin1@users.noreply.github.com>
2021-06-14 20:22:45 +0900

    Add files via upload

---
+++ Process Manager/manualclose.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+managingProcess=$(<.nowexecuting.txt)
+#Script written by Hoplin
+echo "[ $(date +%Y)/$(date +%m)/$(date +%d) $(date +%H):$(date +%M):$(date +%S) ] : Process has been removed manually by the user" >> RestartLog.txt
+pid="ps -ef | grep '$managingProcess' | grep -v 'grep' | grep -v 'bash'"
+value=`eval $pid | awk '{print $2}'` 
+echo `kill -15 $value`
